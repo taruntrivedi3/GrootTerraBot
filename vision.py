a578e2906e4700e60f916cd1bc5b6d4079f43253
Makafanka <80500900+Makafanka@users.noreply.github.com>
2023-11-12 23:21:48 -0500

    Part 4 & Part 2

--- vision.py
+++ vision.py
@@ -16,14 +16,7 @@ def classifyFoliage(image):
     x_image = transformFromBGR(image, x_color_space)
     x_image = cv2.blur(x_image, (3, 3))
     x_foliage_mask = createMask(x_image, x_rVals, x_color_space)
-
-    #y_rVals = [(0, 87), (33, 255), (15, 200)]
-    #y_color_space = 'BGR'
-    #y_image = transformFromBGR(image, color_space)
-    #y_image = cv2.blur(image, (3, 3))
-    #y_foliage_mask = createMask(y_image, y_rVals, y_color_space)
     # END STUDENT CODE
-    #foliage_mask = y_foliage_mask & x_foliage_mask
     return x_foliage_mask
 
 # Return the bounding box of the measuring stick
@@ -32,12 +25,12 @@ def classifyFoliage(image):
 def findStick (image):
     boundingBox = np.array([[0,0]])
     # BEGIN STUDENT CODE
+    boundingBox = np.array([[1960,280], [2046, 280], [1920, 2100], [1846, 2100]])
     # END STUDENT CODE
     return boundingBox
 
 # Find the height of the tallest that occludes (is in front of) the measuring stick.
 # Use the bounding box returned by "findStick" to create a mask, 
-
 
 # Given the foliage mask (as would be returned from classifyFoliage), 
 #   return the height in cms the tallest plant that crosses in front 
@@ -46,15 +39,50 @@ def measureHeight(image, foliage_mask):
     # First, use the bounding box returned from findStick to create a mask
     #   of the measuring stick
     stick_mask = np.zeros(foliage_mask.shape[0:2], np.uint8)
-    # BEGIN STUDENT CODE
+    # BEGIN STUDENT CODE    
+    contour = findStick(image)
+    
+    cv2.drawContours(stick_mask, [contour], 0, 255, -1)
+    
+    combined_mask = cv2.bitwise_and(foliage_mask, stick_mask)
+    height_list = [360, 585, 820, 1020, 1215, 1390, 1560, 1720, 1870, 2000]
+
     # END STUDENT CODE
 
     # Find the maximum height of plants that overlap the measuring stick
     #   in the foliage_mask
-    height = top_row = None
     # BEGIN STUDENT CODE
+    nonzero_points = np.argwhere(combined_mask > 0)
+
+    if len(nonzero_points) == 0: 
+        return None, None 
+     
+    else: 
+        #highest_point = nonzero_points[np.argmax(nonzero_points[:, 1])]
+        highest_point = nonzero_points[np.argmin(nonzero_points[:, 0])]
+        highest_point_row = highest_point[0]
+        height_fin = highest_point_row
+
+        #height, top_row = None
+        top_row = highest_point_row
+        height = 0 
+        
+        for i in range(len(height_list)):
+            if (i > 0 and height_fin >= height_list[i]): 
+                stick_diff = height_list[i-1] - height_list[i]
+
+                foilage_height = height_fin - height_list[i]
+                #print("foilage_height", foilage_height)
+                height = ((9-i) + (foilage_height/stick_diff))
+            elif height_fin >= height_list[0] and height_fin < height_list[1]:
+                stick_diff = height_list[0] - height_list[1]
+                foilage_height = height_fin - height_list[0]
+                height = ((9-i) + (foilage_height/stick_diff))
+        if height == 0:
+            top_row = contour[3][1]
     # END STUDENT CODE
-    return height, top_row
+        return height, top_row
+    
 
 # Use the color calibration squares to find a transformation that will
 #   color-correct the image such that the mean values of the calibration
@@ -99,9 +127,32 @@ def foliageImages (image):
     height, row = measureHeight(image, foliage_mask)
     foliageImage = None
     # BEGIN STUDENT CODE
+    foliage_mask = cv2.cvtColor(foliage_mask, cv2.COLOR_GRAY2BGR)
+    foliageImage = cv2.bitwise_and(image,foliage_mask)
+    boundingBox = findStick(image)
+    #image = cv2.polylines(image, [boundingBox], isClosed=True, color=(255, 0, 0), thickness=10)
+    image = cv2.line(image, boundingBox[0], boundingBox[1], color=(255, 0, 0), thickness=10)
+    image = cv2.line(image, boundingBox[1], boundingBox[2], color=(255, 0, 0), thickness=10)
+    image = cv2.line(image, boundingBox[2], boundingBox[3], color=(255, 0, 0), thickness=10)
+    image = cv2.line(image, boundingBox[3], boundingBox[0], color=(255, 0, 0), thickness=10)
+    intersection = []
+    edges = [(boundingBox[1], boundingBox[2]), (boundingBox[3], boundingBox[0])]
+    for pt1, pt2 in edges:
+        if min(pt1[1], pt2[1]) <= row <= max(pt1[1], pt2[1]):
+    	    inter = int(pt1[0]+(row-pt1[1])*(pt2[0]-pt1[0])/(pt2[1]-pt1[1]))
+    	    intersection.append(inter)
+    if len(intersection) == 0:
+        start_point = boundingBox[3][0], row
+        end_point = boundingBox[1][0], row
+    else:
+        start_point = (intersection[1]-20, row)
+        end_point = (intersection[0]+20, row)
+    image = cv2.line(image, start_point, end_point, color=(0, 0, 255), thickness=10)
     # END STUDENT CODE
     return foliageImage, image, height
 
+last_height = 0
+last_greenery = 0
 # Given an image, return two values:
 # 1. The amount of foliage in the image
 # 2. An estimate of the plant health (as a string), based on changes from the
@@ -112,6 +163,26 @@ def plantHealth (image):
     greenery = 0
     health_msg = ""
     # BEGIN STUDENT CODE
+    global last_height, last_greenery
+    greenery = cv2.countNonZero(foliage_mask)/(image.shape[0]*image.shape[1])
+    if last_height == 0 or last_greenery == 0:
+        if height > 0 or greenery > 0:
+            health_msg = "good"
+        elif height < 0 or greenery < 0:
+            health_msg = "bad"
+        else:
+            health_msg = "ok"
+    else:
+        height_change = (height-last_height)/last_height
+        volumn_change = (greenery-last_greenery)/last_greenery
+        if height_change > 0.05 or volumn_change > 0.05:
+    	    health_msg = "good"
+        elif height_change < 0 or volumn_change < 0:
+    	    health_msg = "bad"
+        else:
+    	    health_msg = "ok"
+    last_greenery = greenery
+    last_height = height
     # END STUDENT CODE
     return greenery, health_msg
 
