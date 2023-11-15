09b7404748d88190ad1e671d214efcf2b8df57ad
timward13 <92277403+timward13@users.noreply.github.com>
2023-11-15 14:18:32 -0500

    writes to water.txt

--- greenhouse_behaviors.py
+++ greenhouse_behaviors.py
@@ -461,6 +461,17 @@ class RaiseSMoist(Behavior):
         self.total_water += dwater
         print("calcWaterAdded: %.1f (%.1f = %.1f - %.1f)"
               %(self.total_water, dwater, self.weight_est, self.start_weight))
+
+	try:
+		with open('water.txt', 'r') as file:
+			daily_total = file.read()
+		daily_total = daily_total.strip()
+		daily_total = float(daily_total)
+		daily_total += dwater
+		with open('water.txt', 'w' as file:
+			file.write(str(daily_total))
+	except:
+		pass
         
     def printWateredEnough(self):
         print("Watered Enough: %.1f" %self.total_water)
